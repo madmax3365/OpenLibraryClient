cf2eaa25113238fdbacd4a13b92141aac386748f
Gago <gagmuradyan93@gmail.com>
2024-07-07 19:20:52 +0400

    ops: Define platform versions, add SwiftLint

--- Package.swift
+++ Package.swift
@@ -5,19 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "OpenLibraryClient",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v16),
+        .watchOS(.v9),
+        .tvOS(.v16),
+        .visionOS(.v1),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OpenLibraryClient",
             targets: ["OpenLibraryClient"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.55.1"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "OpenLibraryClient"),
+            name: "OpenLibraryClient",
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]),
         .testTarget(
             name: "OpenLibraryClientTests",
             dependencies: ["OpenLibraryClient"]),
-    ]
-)
+    ])
